9ddff88cad5591dd288c29bfad9682172654c6ca
Miguel Divo <migueldivo11@gmail.com>
2022-11-17 15:17:44 +0000

    testing game smoothness

--- Assets/MovementAI.cs
+++ Assets/MovementAI.cs
@@ -83,7 +83,9 @@ public class MovementAI : MonoBehaviour
 
     void UpdateDestination()
     {
-        target = waypoints[waypointIndex].position; //set our target to our current waypoint
+        //target = waypoints[waypointIndex].position; //set our target to our current waypoint
+
+        target = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, Time.deltaTime * 10); //set our target to our current waypoint
         agent.SetDestination(target);               //set the nav mesh agent's destination to target
     }
 
