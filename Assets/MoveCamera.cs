df9d090ecdbb4139f744b9fbcf3b9c7014df8a97
JingSZhuo <jing2001@live.co.uk>
2022-12-23 02:19:09 +0000

    fixed laggy player movement + adjusted speed

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -1,17 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
 
-    public Transform cameraPostion;
+    public Transform cameraPosition;
+    public float smoothSpeed = 1f;
 
 
     // Update is called once per frame
-    private void Update()
-    {
-        transform.position = cameraPostion.position;
-        
+
+    private void LateUpdate() {
+
+        Vector3 desiredPosition = cameraPosition.position;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = desiredPosition;
     }
 }
