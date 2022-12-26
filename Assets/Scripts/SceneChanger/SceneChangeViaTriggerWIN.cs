590c4761540602f4ce3435ce0a0cd1300e8a4d4c
JingSZhuo <jing2001@live.co.uk>
2022-12-26 03:14:16 +0000

    brushed up hierarchy

---
+++ Assets/Scripts/SceneChanger/SceneChangeViaTriggerWIN.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeViaTriggerWIN : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("triggered");
+        SceneManager.LoadScene(2);
+    }
+}
