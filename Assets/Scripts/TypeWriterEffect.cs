f282fa60f6d099467058a6d1e843b0d3626c6d38
Miguel Divo <migueldivo11@gmail.com>
2023-01-01 23:32:09 +0400

    handgun/knife/car asset

--- Assets/Scripts/TypeWriterEffect.cs
+++ Assets/Scripts/TypeWriterEffect.cs
@@ -33,7 +33,7 @@ public class TypeWriterEffect : MonoBehaviour
         for(int i = 0; i < fullText.Length+1; i++)
         {
             currentText = fullText.Substring(0, i);
-            this.GetComponent<TextMeshPro>().text = currentText;
+            this.GetComponent<Text>().text = currentText;
             yield return new WaitForSeconds(delay);
         }
     }
