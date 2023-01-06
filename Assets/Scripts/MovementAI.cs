7bb406c61f1b30a72651a479d313df0777815a08
kalina-ivanova28 <78103096+kalina-ivanova28@users.noreply.github.com>
2023-01-06 22:32:18 +0000

    furniture fix, puzzle complete and waypoints

--- Assets/Scripts/MovementAI.cs
+++ Assets/Scripts/MovementAI.cs
@@ -41,7 +41,7 @@ public class MovementAI : MonoBehaviour
 
     //Random numbers for Puzzle 1
     public bool flag = false;
-    float number = 1;
+    float number;
 
     // Start is called before the first frame update
     void Start()
@@ -50,6 +50,7 @@ public class MovementAI : MonoBehaviour
         textMeshPro_instructions = instructions.GetComponent<TextMeshProUGUI>();
         textMeshPro_outcome = Outcome.GetComponent<TextMeshProUGUI>();
         agent = GetComponent<NavMeshAgent>();
+        number = Random.Range(0,9);
         
 
         //Weapon GUI
@@ -69,7 +70,7 @@ public class MovementAI : MonoBehaviour
             {
                 IterateWaypointIndex();         //we are going to increase the waypoint index by 1
                 UpdateDestination();            //updates the destination waypoint
-                
+                Debug.Log("<2");
             }
             else if(waypointIndex == 2){                                              //Entrance choice
                 textMeshPro_Weapon_1.text = weapon_1;
@@ -83,6 +84,7 @@ public class MovementAI : MonoBehaviour
                     IterateWaypointIndex();         //+1 to waypoint index
                     UpdateDestination();
                 }
+                Debug.Log("2");
             }
             else if(waypointIndex == 4)                                               //First Choice after entering
             {
@@ -104,12 +106,7 @@ public class MovementAI : MonoBehaviour
                         //IterateWaypointIndex();
                         UpdateDestination();
                     }
-            }
-            else if(waypointIndex == 15)
-            {
-                    Debug.Log("Waypoint 15 reached");
-                    //GuessingGame();    
-
+                    Debug.Log("4");
             }
             else if(waypointIndex == 7)                                               //waypoint 6
             {
@@ -132,8 +129,9 @@ public class MovementAI : MonoBehaviour
                         IterateWaypointIndex();        //This sometimes skips some waypoints...not needed in every if statement
                         UpdateDestination();
                     }
+                    Debug.Log("7");
             }
-            else if(waypointIndex == 12)                                               //
+            else if(waypointIndex == 12)                                               
             {
                 textMeshPro_click.text = click_value;
                 textMeshPro_instructions.text = instructions_value;
@@ -153,6 +151,7 @@ public class MovementAI : MonoBehaviour
                     //IterateWaypointIndex();
                     UpdateDestination();
                 }
+                Debug.Log("12");
             }
             else if (waypointIndex == 22){
                 textMeshPro_click.text = click_value;
@@ -164,7 +163,7 @@ public class MovementAI : MonoBehaviour
                     waypointIndex=23;
                     // IterateWaypointIndex();
                     UpdateDestination();
-                    Debug.Log("Got here I think");
+                    //Debug.Log("Got here I think");
                 }
                 else if (Input.GetKeyDown(KeyCode.Alpha2))
                 {
@@ -173,16 +172,17 @@ public class MovementAI : MonoBehaviour
                     waypointIndex = 24;
                     //IterateWaypointIndex();
                     UpdateDestination();
-                    Debug.Log("The problem is here");
+                    //Debug.Log("The problem is here");
                 }
+                Debug.Log("22");
             }
-            // else if(waypointIndex == 23){
+            // else if(waypointIndex == 24){
             //     waypointIndex = 25;
-            //     Debug.Log("It got here");
             //     UpdateDestination();
+            //     Debug.Log("It got here");
             // } 
             else if(waypointIndex == 26){
-                Debug.Log("Here");
+                //Debug.Log("Here");
                 textMeshPro_click.text = click_value;
                 textMeshPro_instructions.text = instructions_value;
                 if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -201,15 +201,22 @@ public class MovementAI : MonoBehaviour
                     IterateWaypointIndex();
                     UpdateDestination();
                 }
+                Debug.Log("26");
             }
             else if (waypointIndex == 27){
                 waypointIndex = 21;
                 UpdateDestination();
+                Debug.Log("27");
             }
             // else if(waypointIndex == 29)     
             // {
             //     Debug.Log("Just a function to prevent OutOFBoundsIndex Error...TBC...");
             // }
+            else if(waypointIndex == 28){
+                Debug.Log("Waypoint 28 reached");
+                GuessingGame();
+                Debug.Log("28"); 
+            }
             else{                       //if there is no input required... it will follow any remaining waypoints 
                 Debug.Log("Else statement initiated");
                 IterateWaypointIndex();
@@ -236,9 +243,9 @@ public class MovementAI : MonoBehaviour
     }
     void GuessingGame()
     {
-        //number = Random.Range(0,2);
+        //number = Random.Range(0,9);
         Debug.Log("Number generated: " + number);
-        textMeshPro_instructions.text = "Choose a number between 1 - 9";
+        textMeshPro_instructions.text = "Choose a number between 0 - 9";
         if (flag == false)
             {
                 if(Input.GetKeyDown(KeyCode.Alpha0) && number == 0){
@@ -262,28 +269,54 @@ public class MovementAI : MonoBehaviour
                     IterateWaypointIndex();
                     UpdateDestination();
                 }
-
-
                 else if(Input.GetKeyDown(KeyCode.Alpha3) && number == 3){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha4) && number == 4){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha5) && number == 5){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha6) && number == 6){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha7) && number == 7){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha8) && number == 8){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else if(Input.GetKeyDown(KeyCode.Alpha9) && number == 9){
                     flag=true;
+                    textMeshPro_instructions.text = empty;
+                    Debug.Log("Correct!");
+                    IterateWaypointIndex();
+                    UpdateDestination();
                 }
                 else{
                     textMeshPro_outcome.text=wrong;
